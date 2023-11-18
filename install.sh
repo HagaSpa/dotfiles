a3e296ddcb18680c6953ff474443e7cc72b89d53
HagaSpa <justicesparrow13@gmail.com>
2023-11-18 14:38:24 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zprofile
+
+# install via brew
+brew bundle --file=./Brewfile
